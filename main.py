c883d836b99e46fe062c10ec30aba5155d7f464c
KDKftw <ooo.kadoun@gmail.com>
2020-09-15 22:16:05 +0200

    all functions are up

--- main.py
+++ main.py
@@ -20,7 +20,64 @@ def moneta():
     print(monprice)
 
 
+def philipM():
+    url = ("https://www.patria.cz/akcie/TABKbl.PR/philip-morris-cr/online.html")
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    philipMprice = soup.find("td", class_="EquityHeaderCellStrong").text
+    print(philipMprice)
+
+def KB():
+    url = ("https://www.patria.cz/akcie/BKOMbl.PR/komercni-banka/online.html")
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    KBprice = soup.find("td", class_="EquityHeaderCellStrong").text
+    print(KBprice)
+
+def o2():
+    url = ("https://www.patria.cz/akcie/SPTTbl.PR/o2-cr/online.html")
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    o2price = soup.find("td", class_="EquityHeaderCellStrong").text
+    print(o2price)
+
+def avast():
+    url = ("https://www.patria.cz/akcie/6c4aaf22-3613-4221-b81b-ae4957004c0d/avast-rg/online.html")
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    avastPrice = soup.find("td", class_="EquityHeaderCellStrong").text
+    print(avastPrice)
+
+def erste():
+    url = ("https://www.patria.cz/akcie/ERSTbl.PR/erste-bank/online.html")
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    erstePrice = soup.find("td", class_="EquityHeaderCellStrong").text
+    print(erstePrice)
+
+def cez():
+    url = ("https://www.patria.cz/akcie/CEZPbl.PR/cez/online.html")
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    cezPrice = soup.find("td", class_="EquityHeaderCellStrong").text
+    print(cezPrice)
+
+def vig():
+    url = ("https://www.patria.cz/akcie/VIGRbl.PR/vig/online.html")
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    vigPrice = soup.find("td", class_="EquityHeaderCellStrong").text
+    print(vigPrice)
+
+vig()
+cez()
+erste()
+avast()
+o2()
+KB()
+philipM()
 moneta()
 
 import PySimpleGUI as sg
+
 sg.Window(title="StocksWatcher", layout=[[]], margins=(400, 50)).read()
